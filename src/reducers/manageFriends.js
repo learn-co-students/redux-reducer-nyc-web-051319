fcdc64c8bfcd734ebb6a8a5f6d7f37efe3b04988
Will Bainton <baintonw@gmail.com>
2019-08-01 13:03:42 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [ { name: 'Avi', hometown: 'NYC', id: 100 } ] }, action){
+  switch(action.type){
+    case "ADD_FRIEND":
+      return {...state, friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+        const index = state.friends.findIndex(friend=> {
+          return friend.id === action.id
+        })
+        return {...state, friends: [
+          ...state.friends.slice(0, index),
+          ...state.friends.slice(index + 1)
+        ]}
+    default:
+      return state
+  }
 }
